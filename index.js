425310581bf1d391e97c20bf41cd7cfb0d2102c5
Jeroen Engels <jfm.engels@gmail.com>
2017-11-22 17:07:22 +0100

    Fix: Running binary causes two lambdas to run

--- index.js
+++ index.js
@@ -94,18 +94,3 @@ const start = ({lambdaFile, lambdaHandler, kinesisEndpoint, kinesisStream}) => {
 module.exports = {
   start
 };
-
-if (!process.parent) {
-  const argv = minimist(process.argv.slice(2));
-
-  const {
-    'lambda-file': lambdaFile,
-    'lambda-handler': lambdaHandler,
-    'kinesis-endpoint': kinesisEndpoint,
-    'kinesis-stream': kinesisStream
-  } = argv;
-
-  const close = start({lambdaFile, lambdaHandler, kinesisEndpoint, kinesisStream});
-
-  process.on('SIGTERM', close);
-}
