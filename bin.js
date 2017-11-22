a6041cc50cf402db3c6a518c9a57ff78e2cdcc4d
Jeroen Engels <jfm.engels@gmail.com>
2017-11-22 17:08:25 +0100

    Make binary relaunch lambda when it crashes

--- bin.js
+++ bin.js
@@ -2,17 +2,30 @@
 const minimist = require('minimist');
 const {start} = require('.');
 
-if (!process.parent) {
-  const argv = minimist(process.argv.slice(1));
+const argv = minimist(process.argv.slice(1));
 
-  const {
-    'lambda-file': lambdaFile,
-    'lambda-handler': lambdaHandler,
-    'kinesis-endpoint': kinesisEndpoint,
-    'kinesis-stream': kinesisStream
-  } = argv;
+const {
+  'lambda-file': lambdaFile,
+  'lambda-handler': lambdaHandler,
+  'kinesis-endpoint': kinesisEndpoint,
+  'kinesis-stream': kinesisStream
+} = argv;
 
-  const close = start({lambdaFile, lambdaHandler, kinesisEndpoint, kinesisStream});
+let close = start({lambdaFile, lambdaHandler, kinesisEndpoint, kinesisStream});
 
-  process.on('SIGTERM', close);
+async function relaunchLambda() {
+  console.log('Closing lambda');
+  await close();
+  console.log('Relaunching lambda');
+  close = start({lambdaFile, lambdaHandler, kinesisEndpoint, kinesisStream});
 }
+
+process.on('SIGTERM', () => close());
+process.on('uncaughtException', error => {
+  console.error('Uncaught exception: ', error);
+  relaunchLambda();
+});
+process.on('unhandledRejection', (reason, p) => {
+  console.error('Unhandled rejection at:', p, 'reason:', reason);
+  relaunchLambda();
+});
